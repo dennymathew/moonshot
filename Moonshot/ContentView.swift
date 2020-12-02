43343e66bdde0d3ebf11037baf80021bfb1bc575
Denny Mathew <devdenmathew@gmail.com>
2020-12-02 20:52:26 +0530

    Finishes master screen.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -6,11 +6,34 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
+struct MissionView: View {
+    let mission: Mission
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image(mission.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 44, height: 44)
+            VStack(alignment: .leading) {
+                Text(mission.displayName)
+                    .font(.headline)
+                Text(mission.formattedLaunchDate)
+            }
+        }
+    }
+}
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    MissionView(mission: mission)
+                }
+            }
+            .navigationBarTitle("MoonShot")
+        }
     }
 }
 
